ad0d381b5c793b450a48908bf258b5646cae74f5
Phil Jin Sang <philjin97@gmail.com>
2023-01-09 13:58:44 +0900

    Added input.py

---
+++ 입출력.py
@@ -0,0 +1,41 @@
+# # 1. 
+# number = int(input())
+# print(number)
+
+# # 2. 
+# a, b = list(map(int, input().split()))
+# print(a, b)
+
+# # 3. 
+# numbers = list(map(int,input().split()))
+# print(*numbers) # * = unpacking. 
+
+# 4. 
+# string = input().split()
+# for s in string:
+#     print(s,end=' ')
+
+# # 5. 
+# numbers = list(map(int, input().split()))
+# for number in numbers:
+#     print(number, end=' ')
+
+# # 6. 
+# a, b = list(map(int, input().split()))
+# print(a, b)
+
+# 7. 
+string = input().split()
+for s in string:
+    print(s, end=' ')
+
+# 8. 
+numbers = list(map(int, input().split()))
+for number in numbers:
+    print(number, end=' ')
+
+# 9. 
+numbers = list(map(int, input().split()))
+for number in numbers:
+    print(number, end=' ')
+
