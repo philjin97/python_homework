8e379ee2a159c08e32287b95af3bb31aea9d22ad
Phil Jin Sang <philjin97@gmail.com>
2023-01-10 09:46:37 +0900

    Modified 구현 problem 6

--- jan9/구현.py
+++ jan9/구현.py
@@ -1,61 +1,63 @@
-# 1. 
-n = -1
+# # 1. 
+# n = -1
 
-string = input('Input a String: ')
-for s in range(len(string)):
-    if string[s] == 'e':
-        n = s
-        break
-print(n)
+# string = input('Input a String: ')
+# for s in range(len(string)):
+#     if string[s] == 'e':
+#         n = s
+#         break
+# print(n)
 
-# 2. 
-wd = {}
+# # 2. 
+# wd = {}
 
-words = input().split()
-for n in words:
-    if n in wd:
-        wd[n] += 1
-    else:
-        wd[n] = 1
+# words = input().split()
+# for n in words:
+#     if n in wd:
+#         wd[n] += 1
+#     else:
+#         wd[n] = 1
 
-for k, v in wd.items():
-    print(k, v)
+# for k, v in wd.items():
+#     print(k, v)
 
-# 3. 
-without = ''
+# # 3. 
+# without = ''
 
-string = input('Input a String: ')
-for s in string:
-    if s != 'e':
-        without += s
-print(without)
+# string = input('Input a String: ')
+# for s in string:
+#     if s != 'e':
+#         without += s
+# print(without)
 
-# 4. 
-n = 0
+# # 4. 
+# n = 0
 
-string = input().split()
-for s in string[0]:
-    if s == string[1]:
-        n += 1
-print(n)
+# string = input().split()
+# for s in string[0]:
+#     if s == string[1]:
+#         n += 1
+# print(n)
 
-# 5. 
-string = ''
-numbers = input().split()
-for n in range(len(numbers)):
-    if n == 0:
-        string += numbers[0]
-    else:
-        string += '-' + numbers[n]
-print(string)
+# # 5. 
+# string = ''
+# numbers = input().split()
+# for n in range(len(numbers)):
+#     if n == 0:
+#         string += numbers[0]
+#     else:
+#         string += '-' + numbers[n]
+# print(string)
 
 # 6. 다시 풀어보기 
-# n = 0 
-# number = int(input())
+number = int(input())
+n = 0 
 
-# while number > 0:
-#     n += number%10
-#     n //= 10
+while number > 0:
+    n += number % 10
+    number //= 10
+
+print(n)
 
 
 
