aaabe8c382c860e77aabfeea911d877c740e15f3
Phil Jin Sang <philjin97@gmail.com>
2023-01-10 14:00:13 +0900

    Added 입출력.py

---
+++ jan10/입출력.py
@@ -0,0 +1,42 @@
+# # 1. 
+# num = int(input())
+# print(num)
+
+# 2. 
+words = input().split()
+for word in words:
+    print(word, end=' ')
+
+# 3. 
+T = int(input())
+
+for t in range(1, T+1):
+    print(t)
+
+# 4. 
+numbers = list(map(int, input().split()))
+
+for number in numbers:
+    print(number, end=' ')
+
+# 5. 
+a, b = list(map(int, input().split()))
+print(a, b)
+
+# 6. 
+string = input().split()
+print(string)
+
+# 7. 
+T = int(input())
+
+for t in range(1, T+1):
+    numbers = list(map(int, input().split()))
+    print(numbers)
+
+# 8. 
+T = int(input())
+
+for t in range(1, T+1):
+    numbers = list(map(int, input().split()))
+    print(numbers)
